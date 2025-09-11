8a648f25c9ddfbd55baffb57bd7e3c24b8efd80a
Aansh patel <118075566+anshp-30304@users.noreply.github.com>
2025-09-11 12:59:23 +1000

    fixed connection to dashboard

--- AgDeskDjango/Treeminder/urls.py
+++ AgDeskDjango/Treeminder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='treeminder_dashboard'),
     path('header/', views.dashboard_header, name='dashboard_header'),
     path('stats/', views.stats_cards, name='stats_cards'),
     path('tree-status/', views.tree_status, name='tree_status'),
